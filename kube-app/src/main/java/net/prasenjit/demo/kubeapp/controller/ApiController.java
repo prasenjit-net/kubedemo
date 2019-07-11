6b02cd9d7286190d45c42aa46ff7dd7523eb1963
prasenjit-net <github@prasenjit.net>
2019-07-12 00:22:17 +0530

    Some correction on demo application

--- kube-app/src/main/java/net/prasenjit/demo/kubeapp/controller/ApiController.java
+++ kube-app/src/main/java/net/prasenjit/demo/kubeapp/controller/ApiController.java
@@ -1,19 +1,27 @@
 package net.prasenjit.demo.kubeapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import net.prasenjit.demo.kubeapp.model.ApiMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ApiController {
 
     @Value("${spring.cloud.client.ip-address}")
     private String ip;
 
-    @GetMapping("/")
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/greeting")
     public ApiMessage message(@RequestParam(defaultValue = "World") String name) {
-        return new ApiMessage("Hello " + name + "! from " + ip);
+        log.info("KUBE-APP called with name=" + name);
+        return new ApiMessage("Hello " + name + "! from " + ip, discoveryClient.getServices());
     }
 }
--- kube-app/src/main/java/net/prasenjit/demo/kubeapp/model/ApiMessage.java
+++ kube-app/src/main/java/net/prasenjit/demo/kubeapp/model/ApiMessage.java
@@ -2,7 +2,10 @@ package net.prasenjit.demo.kubeapp.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class ApiMessage {
     private final String message;
+    private final List<String> services;
 }
