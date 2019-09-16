1ee5ede2a00f9f76e9330eea4b01c5a0599016fb
prasenjit-net <github@prasenjit.net>
2019-09-16 20:38:24 +0530

    forcefully enabled due to a bug in present code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# kubedemo
+# Kubernetes Demo
 A example project to demonstrate microservice in kubernetes
 ## An example project
 This is an example project the deployment topology of spring boot microservice in kubernetes.
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-if $1 == 'i'; then
-    echo 'Installing'
-elif $1 == 'u' then
-    echo 'Uninstalling'
+if ["$1" == "i"];
 then
+    echo "Installing"
+elif ["$1" == "u"];
+then
+    echo "Uninstalling"
+fi
